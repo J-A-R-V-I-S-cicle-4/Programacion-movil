1ff1fb094e28b252ed1b292dae0f5580362f2329
andresmolinasix <andres.molina@urosario.edu.co>
2021-11-16 19:13:23 -0500

    ADD Interfaz Andres Molina

---
+++ app/src/main/java/com/andresmolinasix/andresmolina/gorgona.kt
@@ -0,0 +1,11 @@
+package com.andresmolinasix.andresmolina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gorgona : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gorgona)
+    }
+}
---
+++ app/src/main/java/com/andresmolinasix/andresmolina/picoDelLoro.kt
@@ -0,0 +1,11 @@
+package com.andresmolinasix.andresmolina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class picoDelLoro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pico_del_loro)
+    }
+}
