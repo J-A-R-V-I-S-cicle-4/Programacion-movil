c67d7193ad2f3fa407eed014f06dcf3c7e26dbef
Eliana N <eliana.navarro@uptc.edu.co>
2021-11-19 09:40:14 -0500

    Sprint 2 recyclerview  J.A.R.V.I.S

---
+++ app/src/main/java/com/eliana/sanantero/ListPoiActivity.kt
@@ -0,0 +1,11 @@
+package com.eliana.sanantero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPoiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_poi)
+    }
+}
---
+++ app/src/main/java/com/eliana/sanantero/SitioInteresActivity.kt
@@ -0,0 +1,11 @@
+package com.eliana.sanantero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SitioInteresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sitio_interes)
+    }
+}
